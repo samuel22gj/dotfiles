c9539d0a41de4618b631ad0f98611a7956f2dd9d
samuel22gj <samuel22gj@gmail.com>
2016-03-07 18:36:24 +0800

    [homebrew] Linux don't need it

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,24 +1,21 @@
-#!/bin/sh
-#
+#!/usr/bin/env bash
+set -e
+
 # Homebrew
 #
 # This installs some of the common dependencies needed (or at least desired)
 # using Homebrew.
 
-# Check for Homebrew
-if test ! $(which brew)
+# Check OS
+if test "$(uname -s)" = "Darwin"
 then
-  echo "  Installing Homebrew for you."
 
-  # Install the correct homebrew for each OS type
-  if test "$(uname -s)" = "Darwin"
+  # Check for Homebrew
+  if test ! $(which brew)
   then
+    echo "  Installing Homebrew for you."
     ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  elif test "$(expr substr $(uname -s) 1 5)" = "Linux"
-  then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/linuxbrew/go/install)"
   fi
-
 fi
 
 exit 0
