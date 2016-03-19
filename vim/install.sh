303a93ba65cd1bbc2de5788b5950ecda81e72ab4
samuel22gj <samuel22gj@gmail.com>
2016-03-19 16:24:46 +0800

    [vim] Update vimrc and install vundle

---
+++ vim/install.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#
+# Vim
+#
+# Install Vundle, Vim plugin manager.
+
+set -e
+
+export DOTFILES=$HOME/.dotfiles
+
+# Include customize printf functions
+source $DOTFILES/script/printf
+
+# Check for Vundle
+if [ ! -d ~/.vim/bundle ]
+then
+  info "Installing Vundle"
+  mkdir -p ~/.vim/bundle
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+  vim +PluginInstall +qall
+fi
