ad463cc17975c1d5d01bb5bdf6efab29e378ffdc
samuel22gj <samuel22gj@gmail.com>
2016-03-24 17:27:33 +0800

    [powerline-fonts] First commit

---
+++ powerline-fonts/install.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+#
+# Powerline fonts
+#
+# Patched fonts for Powerline users.
+
+set -e
+
+export DOTFILES=$HOME/.dotfiles
+
+# Include customize printf functions
+source $DOTFILES/script/printf
+
+install_fonts () {
+  info "Installing Powerline fonts"
+  git clone https://github.com/powerline/fonts powerline-fonts
+  ./powerline-fonts/install.sh
+  rm -rf powerline-fonts
+}
+
+if [[ `uname` == 'Darwin' ]]; then
+  # MacOS
+  font_dir="$HOME/Library/Fonts"
+
+  if [ ! -f "$font_dir/Ubuntu Mono derivative Powerline.ttf" ]
+  then
+    install_fonts
+  fi
+else
+  # Linux
+  font_dir="$HOME/.local/share/fonts"
+
+  if [ ! -f "$font_dir/Ubuntu Mono derivative Powerline.ttf" ]
+  then
+    install_fonts
+  fi
+fi
