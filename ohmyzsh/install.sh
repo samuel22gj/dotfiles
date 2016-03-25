83ef54d19d27a1f2d2d7d673c04a48d145f8f656
Samuel <samuel22gj@gmail.com>
2016-03-25 14:54:29 +0800

    [ohmyzsh] Change shell to zsh after installing oh-my-zsh

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -16,4 +16,5 @@ if [ ! -d ~/.oh-my-zsh ]
 then
   info "Installing oh-my-zsh"
   sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  sudo chsh -s $(which zsh)
 fi
