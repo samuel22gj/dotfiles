f13e6a5aca2893e62bc9b95309c81e46f1c57189
Samuel <samuel22gj@gmail.com>
2016-03-25 14:17:19 +0800

    [ohmyzsh] use sudo to avoid "Password: chsh: PAM authentication failed"

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -15,5 +15,5 @@ source $DOTFILES/script/printf
 if [ ! -d ~/.oh-my-zsh ]
 then
   info "Installing oh-my-zsh"
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 fi
