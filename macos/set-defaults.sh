ed80eee70111029c91d0699d7a9a439f6e929ace
samuel22gj <samuel22gj@gmail.com>
2016-11-18 14:29:15 +0800

    Using macos replace of osx

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 # The original idea (and a couple settings) were grabbed from:
-#   https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+# Keep-alive: update existing `sudo` time stamp until `.macos` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
