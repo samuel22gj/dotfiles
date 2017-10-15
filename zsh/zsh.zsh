88defab07adcd563f41f3644802d66bf8fb50491
samuel22gj <samuel22gj@gmail.com>
2017-10-15 12:39:53 +0800

    oh-my-zsh add zsh-autosuggestions plugin

--- zsh/zsh.zsh
+++ zsh/zsh.zsh
@@ -1,4 +1,5 @@
 fpath=($DOTFILES/functions $fpath)
 fpath=(/usr/local/share/zsh-completions $fpath)
+source /usr/local/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
 autoload -U $DOTFILES/functions/*(:t)
