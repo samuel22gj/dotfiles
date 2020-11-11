c6b11d6042881cf0094d85ed0a927c2308719c73
samuel22gj <samuel22gj@gmail.com>
2020-11-12 00:08:25 +0800

    [android] Add ANDROID_HOME into PATH

--- android/android.zsh
+++ android/android.zsh
@@ -1,11 +1,13 @@
 if [[ `uname` == 'Darwin' ]]; then
-  # OS X
+  # macOS
+  export ANDROID_HOME=~/Library/Android/sdk
   export ANDROID_SDK=~/Library/Android/sdk
   export ANDROID_NDK=~/Library/Android/sdk/ndk-bundle
 else
   # Linux
+  export ANDROID_HOME=~/Android/Sdk/
   export ANDROID_SDK=~/Android/sdk
   export ANDROID_NDK=~/Android/ndk
 fi
 
-export PATH=$PATH:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools:$ANDROID_NDK
+export PATH=$PATH:$ANDROID_SDK/platform-tools:$ANDROID_SDK/tools:$ANDROID_NDK:$ANDROID_HOME
