15da3838c6ce2539c0390c9894d9acb17caae16b
samuel22gj <samuel22gj@gmail.com>
2021-05-05 10:29:51 +0800

    [android] Add ANDROID_PREFS_ROOT into PATH

--- android/android.zsh
+++ android/android.zsh
@@ -1,12 +1,14 @@
 if [[ `uname` == 'Darwin' ]]; then
   # macOS
   export ANDROID_HOME=~/Library/Android/sdk
+  export ANDROID_PREFS_ROOT=~/Library/Android/sdk
   export ANDROID_SDK=~/Library/Android/sdk
   export ANDROID_NDK=~/Library/Android/sdk/ndk-bundle
 else
   # Linux
   export ANDROID_HOME=~/Android/Sdk/
-  export ANDROID_SDK=~/Android/sdk
+  export ANDROID_PREFS_ROOT=~/Android/Sdk
+  export ANDROID_SDK=~/Android/Sdk
   export ANDROID_NDK=~/Android/ndk
 fi
 
