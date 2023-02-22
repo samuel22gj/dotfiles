89562bed878f48ffd529a615612cd793111db9e2
Samuel <samuel22gj@gmail.com>
2023-02-22 14:58:15 +0800

    [oh-my-zsh] Update install URL

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -15,7 +15,7 @@ source $DOTFILES/script/printf
 if [ ! -d ~/.oh-my-zsh ]
 then
   info "Installing oh-my-zsh"
-  sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  sudo sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   command -v zsh | sudo tee -a /etc/shells
   sudo chsh -s $(which zsh) $USER
   info "Caution! Re-run bootstrap to override .zshrc"
