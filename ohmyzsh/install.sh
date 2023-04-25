eb0f2a2f646580a968a5bca321de0b998ec099ec
Samuel Huang <samuel22gj@gmail.com>
2023-04-25 15:16:39 +0800

    [zsh] Add plugin: zsh-autosuggestions

--- ohmyzsh/install.sh
+++ ohmyzsh/install.sh
@@ -19,6 +19,7 @@ then
 
   info "Installing zsh plugins"
   git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
   command -v zsh | sudo tee -a /etc/shells
   chsh -s $(which zsh) $USER
